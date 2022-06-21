21ce7e862fba5932b8ee91128c56c1fe618be7e3
unknown <igor.l85@mail.ru>
2022-06-21 18:02:06 +0300

    Задание 1. Мили

---
+++ src/Miles.java
@@ -0,0 +1,15 @@
+public class Miles {
+
+    public static void main(String[] args) {
+
+        int cost = 1000;         // стоимость билета
+        int bonus = 20;          // значение 1 мили
+        int mile = cost / bonus; // формула рассчета миль
+
+        System.out.println("Количество начисленных милей: " + mile);
+
+
+
+
+    }
+}
